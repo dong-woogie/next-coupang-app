c8c57334866b6d79b011e3ea975d4125add931dc
dong-woogie <com6511@gmail.com>
2024-02-07 15:47:49 +0900

    fix : error

--- src/app/review-product/[id]/ReviewProductClient.tsx
+++ src/app/review-product/[id]/ReviewProductClient.tsx
@@ -1,27 +1,26 @@
-'use client'
-import React, { FormEvent, useState } from 'react'
-import styles from './ReviewProduct.module.scss';
-import { useParams, useRouter } from 'next/navigation';
-import { useSelector } from 'react-redux';
-import { selectUserID, selectUserName } from '@/redux/slice/authSlice';
-import useFetchDocument from '@/hooks/useFetchDocument';
-import { Timestamp, addDoc, collection } from 'firebase/firestore';
-import { db } from '@/firebase/firebase';
-import { toast } from 'react-toastify';
-import Heading from '@/components/heading/Heading';
-import Loader from '@/components/loader/Loader';
-import Image from 'next/image';
-import { Rating } from 'react-simple-star-rating';
-import Button from '@/components/button/Button';
+"use client";
+import React, { FormEvent, useState } from "react";
+import styles from "./ReviewProduct.module.scss";
+import { useParams, useRouter } from "next/navigation";
+import { useSelector } from "react-redux";
+import { selectUserID, selectUserName } from "@/redux/slice/authSlice";
+import useFetchDocument from "@/hooks/useFetchDocument";
+import { Timestamp, addDoc, collection } from "firebase/firestore";
+import { db } from "@/firebase/firebase";
+import { toast } from "react-toastify";
+import Heading from "@/components/heading/Heading";
+import Loader from "@/components/loader/Loader";
+import Image from "next/image";
+import { Rating } from "react-simple-star-rating";
+import Button from "@/components/button/Button";
 
 const ReviewProductClient = () => {
-
   const [rate, setRate] = useState(0);
-  const [review, setReview] = useState('');
+  const [review, setReview] = useState("");
 
   const router = useRouter();
 
-  const { id } = useParams();
+  const { id }: { id: string } = useParams();
   const userID = useSelector(selectUserID);
   const userName = useSelector(selectUserName);
 
@@ -40,43 +39,38 @@ const ReviewProductClient = () => {
       rate,
       review,
       reviewDate: date,
-      createdAt: Timestamp.now().toDate()
-    }
+      createdAt: Timestamp.now().toDate(),
+    };
 
     try {
-      addDoc(collection(db, 'reviews'), reviewData);
+      addDoc(collection(db, "reviews"), reviewData);
 
       router.push(`/product-details/${id}`);
-
     } catch (error) {
       toast.error(getErrorMessage(error));
     }
-
-  }
+  };
 
   return (
     <section className={styles.review}>
       <Heading title="상품평 작성하기" />
       {product === null ? (
         <Loader basic />
-      ) :
-        (
-
-          <div>
-            <p>
-              <b>상품 이름:</b>
-              {product.name}
-            </p>
-            <Image
-              src={product.imageURL}
-              alt={product.name}
-              width={100}
-              height={100}
-              priority
-            />
-          </div>
-        )
-      }
+      ) : (
+        <div>
+          <p>
+            <b>상품 이름:</b>
+            {product.name}
+          </p>
+          <Image
+            src={product.imageURL}
+            alt={product.name}
+            width={100}
+            height={100}
+            priority
+          />
+        </div>
+      )}
       <div className={styles.card}>
         <form onSubmit={(e) => submitReview(e)}>
           <label>평점: </label>
@@ -93,15 +87,12 @@ const ReviewProductClient = () => {
             onChange={(e) => setReview(e.target.value)}
             cols={30}
             rows={10}
-          >
-          </textarea>
-          <Button type="submit">
-            상품평 작성하기
-          </Button>
+          ></textarea>
+          <Button type="submit">상품평 작성하기</Button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ReviewProductClient
+export default ReviewProductClient;
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -5,12 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_CONFIG_API_KEY,
-  authDomain: process.env.FIREBASE_CONFIG_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_CONFIG_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_CONFIG_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_CONFIG_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_CONFIG_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_STORAGE_BUCKET,
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_FIREBASE_CONFIG_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_APP_ID,
 };
 
 // Initialize Firebase
